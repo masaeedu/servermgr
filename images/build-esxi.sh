c0ee596ef995c5a53762df2e5c66999a7c811360
Manea Marius <maneamarius@gmail.com>
2018-02-08 22:09:40 -0500

    update build-esxi.sh script by parameterizing and using mkisofs instead of genisoimage

--- images/build-esxi.sh
+++ images/build-esxi.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
+
+# Example usage:
+# ./build-esxi.sh 192.168.10.214 ks.cfg ./esxi5.iso ./esxi5-auto.iso
+
 set -e
 
+IP=$1
+KS_SCRIPT=$2
+INPUT_ISO=$3
+OUTPUT_ISO=$4
 
-WORKDIR=`mktemp -d`
-HTTP_ADDRESS=http://192.168.10.229:8080/ks.cfg
-INPUT_ISO=./esxi.iso
-OUTPUT_ISO=./custom_esxi.iso
-MOUNTPOINT=/mnt/iso
+WORKDIR=$(mktemp -d)
+URL=http://$IP:8080/$KS_SCRIPT
+MOUNTPOINT=/mnt/$(basename $(mktemp -u) | cut -d '.' -f2)
 
 # Extract
 mkdir -p $MOUNTPOINT
@@ -16,7 +22,8 @@ umount $MOUNTPOINT
 rm -rf $MOUNTPOINT
 
 # Customize
-sed "s#kernelopt=runweasel#kernelopt=runweasel ks=$HTTP_ADDRESS#" -i $WORKDIR/boot.cfg
+sed -i "s#kernelopt=runweasel#kernelopt=runweasel ks=$URL#" $WORKDIR/boot.cfg
 
 # Repack
-genisoimage -relaxed-filenames -J -R -o $OUTPUT_ISO -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e efiboot.img -no-emul-boot $WORKDIR
+#genisoimage -relaxed-filenames -J -R -o $OUTPUT_ISO -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e efiboot.img -no-emul-boot $WORKDIR
+mkisofs -relaxed-filenames -J -R -o $OUTPUT_ISO -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table $WORKDIR
